0d4a8f1faf40095e0661ce7963a7a21d51f3d31b
sujithagandluru <168511201+sujithagandluru@users.noreply.github.com>
2025-06-17 10:21:51 +0530

    Add files via upload

---
+++ sc.java
@@ -0,0 +1,18 @@
+
+import java.util.Scanner;
+
+public class sc {
+    
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("enter a value");
+        int a=sc.nextInt();
+        int b=5;
+        int c=a+b;
+        System.out.print(c);
+
+
+
+
+    }
+}
