df3369a7416e6e5325c864b88fff340b8f77e569
sujithagandluru <168511201+sujithagandluru@users.noreply.github.com>
2025-06-17 11:09:52 +0530

    Add files via upload

---
+++ FavoriteCandies.java
@@ -0,0 +1,16 @@
+import java.util.LinkedHashSet;
+public class FavoriteCandies {
+	public static void main(String[] args) {
+		LinkedHashSet<String> candies=new LinkedHashSet<String>();
+		candies.add("choco");
+		candies.add("KT");
+		candies.add("bty");
+		candies.add("DM");
+		candies.add("snc");
+		candies.add("choco");
+		System .out.println(candies);
+		
+				
+	}
+
+}
