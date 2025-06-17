2f01c5685a966c945232ab75442cd2523ecd9ab5
sujithagandluru <168511201+sujithagandluru@users.noreply.github.com>
2025-06-17 09:48:56 +0530

    Add files via upload

---
+++ HashSetWithOjects.java
@@ -0,0 +1,25 @@
+import java.util.HashSet;
+class Student{
+int rollNo;
+String name;
+public Student(int rollNo,String name){
+this.rollNo=rollNo;
+this.name=name;
+}
+public String toString(){
+	return "Roll No:"+rollNo+",Name:"+name;
+}
+}
+public class HashSetWithOjects{
+	public static void main(String[] args){
+	HashSet<Student>StudentSet=new HashSet<>();
+	StudentSet.add(new Student(101,"Alice"));
+	StudentSet.add(new Student(102,"Bob"));
+	StudentSet.add(new Student(101,"Alice"));
+	System.out.println("Students in Set:");
+	for(Student s:StudentSet){
+		System.out.println(s);
+	}
+}
+}
+		
