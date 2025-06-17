dddcfedfa9808512ffddd9e10943b682b1e4c887
sujithagandluru <168511201+sujithagandluru@users.noreply.github.com>
2025-06-17 09:40:14 +0530

    Add files via upload

---
+++ AVLTree.java
@@ -0,0 +1,79 @@
+class AVLTree {
+    class Node {
+        int key, height;
+        Node left, right;
+        Node(int d) {
+            key = d;
+            height = 1;
+        }
+    }
+    Node root;
+    int height(Node n) {
+        return (n == null) ? 0 : n.height;
+    }
+    int getBalance(Node n) {
+        return (n == null) ? 0 : height(n.left) - height(n.right);
+    }
+    Node rightRotate(Node y) {
+        Node x = y.left;
+        Node T2 = x.right;
+        x.right = y;
+        y.left = T2;
+        y.height = Math.max(height(y.left), height(y.right)) + 1;
+        x.height = Math.max(height(x.left), height(x.right)) + 1;
+        return x;
+    }
+    Node leftRotate(Node x) {
+        Node y = x.right;
+        Node T2 = y.left;
+        y.left = x;
+        x.right = T2;
+        x.height = Math.max(height(x.left), height(x.right)) + 1;
+        y.height = Math.max(height(y.left), height(y.right)) + 1;
+        return y;
+    }
+    Node insert(Node node, int key) {
+        if (node == null)
+            return new Node(key);
+        if (key < node.key)
+            node.left = insert(node.left, key);
+        else if (key > node.key)
+            node.right = insert(node.right, key);
+        else
+            return node;
+        node.height = 1 + Math.max(height(node.left), height(node.right));
+        int balance = getBalance(node);
+        if (balance > 1 && key < node.left.key)
+            return rightRotate(node);
+        if (balance < -1 && key > node.right.key)
+            return leftRotate(node);
+        if (balance > 1 && key > node.left.key) {
+            node.left = leftRotate(node.left);
+            return rightRotate(node);
+        }
+        if (balance < -1 && key < node.right.key) {
+            node.right = rightRotate(node.right);
+            return leftRotate(node);
+        }
+        return node;
+    }
+    public void insert(int key) {
+        root = insert(root, key);
+    }
+    void inorder(Node node) {
+        if (node != null) {
+            inorder(node.left);
+            System.out.print(node.key + " ");
+            inorder(node.right);
+        }
+    }
+    public static void main(String[] args) {
+        AVLTree tree = new AVLTree();
+        int[] values = {30, 20, 40, 10, 25, 50, 5};
+        for (int val : values)
+            tree.insert(val);
+        System.out.print("Inorder Traversal of AVLTree: ");
+        tree.inorder(tree.root);
+        System.out.println();
+    }
+}
